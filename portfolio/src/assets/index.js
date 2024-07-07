f7325e19081038a84be8cb3d987771bed631f2e3
Alina <alina_tca@yahoo.com>
2024-07-07 15:54:42 +0100

    Further changes for the app set up

---
+++ portfolio/src/assets/index.js
@@ -0,0 +1,17 @@
+import abstract from "./abstract.jpg";
+import leafAndEye from "./leaf-and-eye.jpg";
+import leaf from "./leaf.jpg";
+import logoB from "./logo-black.png";
+import logoW from "./logo-white.png";
+import mockup from "./mock-up.jpg";
+import texture from "./texture.jpg";
+
+export {
+abstract,
+leafAndEye,
+leaf,
+logoB,
+logoW,
+mockup,
+texture
+};
